ca567fcea531bf22e534f39f9407e89d9b1daff0
Aframuz <aframuz@tuta.io>
2023-07-11 17:20:55 -0400

    nothing done

--- 07_Project_A_Robot/main.js
+++ 07_Project_A_Robot/main.js
@@ -20,47 +20,48 @@ const roads = [
 
 // Creates a map object that stores an array of connected
 // nodes.
-function buildGraph(edges) {
-   let graph = Object.create(null);
-   function addEdge(from, to) {
-      if (graph[from] == null) {
-         graph[from] = [to];
-      } else {
-         graph[from].push(to);
-      }
-   }
-   for (let [from, to] of edges.map((r) => r.split("-"))) {
-      addEdge(from, to);
-      addEdge(to, from);
-   }
-   return graph;
+function buildGraph(roads){
+   let graph = Object.create(null)
+
+   roads.forEach(road => {
+      let [from, to] = road.split("-")
+      addPath(graph,from, to)
+      addPath(graph,to, from)
+   })
+
+   return graph
 }
 
-const roadGraph = buildGraph(roads);
+function addPath(graph, from, to) {
+   if (graph[from] === undefined) {
+      graph[from] = [to]
+   } else {
+      graph[from].push(to)
+   }
+}
 
+const roadGraph = buildGraph(roads)
 
 /***************
    THE TASK
 ****************/
-// Condensed village's state down to the minimal
-// set of values that define it
 class VillageState {
    constructor(place, parcels) {
-      this.place = place;
-      this.parcels = parcels;
+      this.place = place
+      this.parcels = parcels
    }
 
    move(destination) {
-      if (!roadGraph[this.place].includes(destination)) {
-         return this;
+      if(!roadGraph[this.place].includes[destination]) {
+         return this
       } else {
-         let parcels = this.parcels
-            .map((p) => {
-               if (p.place != this.place) return p;
-               return { place: destination, address: p.address };
-            })
-            .filter((p) => p.place != p.address);
-         return new VillageState(destination, parcels);
+         let parcels = this.parcels.map(parcel => {
+            if (parcel.place !== this.place) {
+               return parcel
+            }
+            return {place: destination, address: parcel.address}
+         }).filter(parcel => parcel.place !== parcel.address)
+         return new VillageState(destination,parcels)
       }
    }
 }
@@ -71,73 +72,43 @@ class VillageState {
 ****************/
 function runRobot(state, robot, memory) {
    for (let turn = 0; ; turn++) {
-      if (state.parcels.length === 0) {
-         console.log(`Done in ${turn} turns`);
+      if (!state.parcels.length) {
+         console.log(`Done in ${turn} turns`)
          break;
       }
-      let action = robot(state, memory);
-      state = state.move(action.direction);
-      memory = action.memory;
-      console.log(`Moved to ${action.direction}`);
+      let action = robot(state, memory) 
+      state - state.move(action.direction)
+      memory = action.memory
+      console.log(`Moved to ${action.direction}`)
    }
 }
 
-// dumbest strategy
 function randomPick(array) {
    let choice = Math.floor(Math.random() * array.length)
    return array[choice]
 }
-function randomRobot (state) {
-   return {direction:randomPick(roadGraph[state.place])}
+
+function randomRobot(state) {
+   return {direction: randomPick(roadGraph[state.place])}
 }
 
-VillageState.random = function(parcelCount = 5){
+VillageState.random = function (parcelCount =5) {
    let parcels = []
-   for(let i = 0; i < parcelCount; i++){
+   for(let i = 0; i < parcelCount; i++) {
       let address = randomPick(Object.keys(roadGraph))
       let place
       do {
          place = randomPick(Object.keys(roadGraph))
-      }  while (place == address)
-      parcels.push({place, address})
-   }
-   return new VillageState("Post Office", parcels)
-}
+      } while (place === address) {
+         parcels.push({place, address})
+      }
 
+      return new VillageState('Post Office', parcels)
+   }
+}
 /**************************
    THE MAIL TRUCK'S ROUTE
 ***************************/
-function routeRobot(state, memory){
-   if(memory.length == 0) {
-      memory = mailRoute
-   }
-   return {direction: memory[0], memory: memory.slice(1)}
-}
-
 /***************
    PATHFINDING
 ****************/
-function findRoute(graph, from, to){
-   let work = [{at: from, route: []}]
-   for(let i = 0; i < work.length; i++) {
-      let {at, route} = work[i]
-      for(let place of graph[at]){
-         if (place == to) return route.concat(place)
-         if (!work.some(w => w.at === place)) {
-            work.push({at: place, route: route.concat(place)})
-         }
-      }
-   }
-}
-
-function goalOrientedRobot({place, parcels}, route) {
-   if (route.length == 0) {
-      let parcel = parcels[0]
-      if( parcel.place !== place) {
-         route = findRoute(roadGraph, place,parcel.place)
-      } else {
-         route = findRoute(roadGraph, place, parcel.address)
-      }
-   }
-   return {direction: route[0], memory: route.slice(1)}
-}
